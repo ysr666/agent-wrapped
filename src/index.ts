3f8e5a5175eb1fe9cb9d9ad89aea2eacc89b5c54
Shirui <77105194+ysr666@users.noreply.github.com>
2026-08-19 03:03:58 +0800

    feat: export P7 review runner APIs

--- src/index.ts
+++ src/index.ts
@@ -31,3 +31,7 @@ export * from "./ingest/dshFilesystem.js";
 export * from "./evaluation/types.js";
 export * from "./evaluation/benchmark.js";
 export * from "./evaluation/localDsh.js";
+export * from "./review/types.js";
+export * from "./review/workspace.js";
+export * from "./review/reviewer.js";
+export * from "./review/runner.js";
