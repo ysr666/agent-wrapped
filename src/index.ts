c9cb261619a0bc76e4ba22d2f8569c6f4cf22d59
Shirui <77105194+ysr666@users.noreply.github.com>
2026-08-19 00:11:39 +0800

    feat: export CatchphraseClusterer

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from "./core/types.js";
 export * from "./core/quoteScorer.js";
 export * from "./core/facetScorer.js";
+export * from "./core/catchphraseClusterer.js";
 export * from "./core/sessionAnalyzer.js";
