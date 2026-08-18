d590e0207d6c5d1ed265480cffc5f2028ca08778
Shirui <77105194+ysr666@users.noreply.github.com>
2026-08-19 00:25:08 +0800

    chore: export BoomerangDetector

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * from "./core/types.js";
 export * from "./core/quoteScorer.js";
 export * from "./core/facetScorer.js";
 export * from "./core/catchphraseClusterer.js";
+export * from "./core/boomerangDetector.js";
 export * from "./core/sessionAnalyzer.js";
